97fba76509b3ae4c0ae677d4dc2a204e75e6fa5e
Jhennerson Barbosa da Silva <92925449+jhennerson@users.noreply.github.com>
2022-05-15 18:07:54 -0300

    Estilização das páginas de login e cadastro

--- cadastro.php
+++ cadastro.php
@@ -5,24 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-
     <link rel="stylesheet" href="style/reset.css">
-
+    <link rel="stylesheet" href="style/base.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Macondo&display=swap" rel="stylesheet">
+    
     <title>Cadastro</title>
 </head>
 <body>
-    <?php
-        require("config/conexao.php");
-    ?>
+    <main class="cadastro__main">
+        <?php
+            require("config/conexao.php");
+        ?>
 
-    <h1>Cadastro</h1>
-    <form action="actions/cadastrar.php" method="post">
-        <label for="login">Login</label>
-        <input required value="teste" type="text" name="login" id="login">
-        <label for="senha">senha</label>
-        <input required value="teste" type="password" name="senha" id="senha">
-
-        <input type="submit" value="Cadastrar">
-    </form>
+        <h1 class="cadastro__titulo">Cadastro</h1>
+        <form class="cadastro__form" action="actions/cadastrar.php" method="post">
+            <label for="login">Usuário</label>
+            <input required value="teste" type="text" name="login" id="login">
+            <label for="senha">senha</label>
+            <input required value="teste" type="password" name="senha" id="senha">
+            <input class="cadastro__botao" id="cadastro__botao" type="submit" value="Cadastrar">
+        </form>
+    </main>    
 </body>
 </html>
