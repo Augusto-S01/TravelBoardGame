3d922685230d3be5d40013e1f9414717dc3755cd
Jhennerson Barbosa da Silva <92925449+jhennerson@users.noreply.github.com>
2022-05-15 21:03:23 -0300

    Centralização dos pontos dos jogadores

--- actions/dado1.php
+++ actions/dado1.php
@@ -9,9 +9,6 @@
     $_COOKIE['jogador1'] = $casaAtual;
     setcookie("jogador1", $casaAtual ,time() + (30 * 24 * 3600),"/");
     setcookie("rodada", $_COOKIE['rodada'] + 1,time() + (30 * 24 * 3600),"/");
+    setcookie("dado", $dado ,time() + (30 * 24 * 3600),"/");
     header("Location: ../jogo.php");
-
-
-
-
 ?>
